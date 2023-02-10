12f6f376a0c965331c9066b174cbbc268d96bb7c
Lucas-Feat <lucas@nexos.bzh>
2023-02-10 16:45:52 +0100

    fix animation & add input

--- js/script.js
+++ js/script.js
@@ -2,9 +2,27 @@ let goButton = document.getElementsByClassName("start-button").item(0);
 let moon = document.getElementById("moon");
 let rocket = document.getElementById("rocket");
 let ready = document.getElementsByClassName("ready").item(0);
-goButton.addEventListener("click", function () {
-    goButton.classList.add("turn-around");
-    moon.classList.add("moon-landing");
-    // rocket.classList.add("rocket-landing");
-    // ready.classList.add("disparition");
+let title = document.getElementsByTagName("h1").item(0);
+
+
+title.children[2].addEventListener("animationend", (e) => {
+    if (e.animationName === "disparition") title.style.display = "none";
 });
+
+['click'].forEach(event => {
+    document.addEventListener(event, function (e) {
+        console.log(e)
+        goButton.classList.add("turn-around");
+        moon.classList.add("moon-landing");
+        rocket.classList.add("rocket-landing");
+        ready.classList.add("disparition");
+    })
+});
+
+rocket.addEventListener("animationend", (e) => {
+    if (e.animationName === "rocket-landing") {
+        displayEmail();
+    }
+});
+
+
